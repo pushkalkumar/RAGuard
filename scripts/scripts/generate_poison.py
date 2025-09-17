6701a712e0206174b392167a67233b6284210708
ShubhamZala09 <shubham.zala2009@gmail.com>
2025-09-16 20:51:07 -0400

    Update generate_poison.py

--- scripts/scripts/generate_poison.py
+++ scripts/scripts/generate_poison.py
@@ -1,1 +1,31 @@
+python scripts/generate_poison.py \
+    --input datasets/clean/nq.jsonl \
+    --output datasets/poisoned/nq_poisoned_10.jsonl \
+    --poison_ratio 0.1
 
+python scripts/generate_poison.py \
+    --input datasets/clean/beir.jsonl \
+    --output datasets/poisoned/beir_poisoned_10.jsonl \
+    --poison_ratio 0.1
+
+
+python scripts/generate_poison.py \
+    --input datasets/clean/nq.jsonl \
+    --output datasets/poisoned/nq_poisoned_30.jsonl \
+    --poison_ratio 0.3
+
+python scripts/generate_poison.py \
+    --input datasets/clean/beir.jsonl \
+    --output datasets/poisoned/beir_poisoned_30.jsonl \
+    --poison_ratio 0.3
+
+
+python scripts/generate_poison.py \
+    --input datasets/clean/nq.jsonl \
+    --output datasets/poisoned/nq_poisoned_50.jsonl \
+    --poison_ratio 0.5
+
+python scripts/generate_poison.py \
+    --input datasets/clean/beir.jsonl \
+    --output datasets/poisoned/beir_poisoned_50.jsonl \
+    --poison_ratio 0.5
